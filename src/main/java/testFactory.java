e11dbcb850fc540ddde1854a96a7be88ea4e743e
RickLoomans <Rickloomans1@gmail.com>
2022-04-08 09:22:08 +0200

    dino juist naamgeven

--- src/main/java/testFactory.java
+++ src/main/java/testFactory.java
@@ -109,7 +109,7 @@ public class testFactory extends Component implements EntityFactory {
                 .viewWithBBox("monster.png")
                 .type(testTypes.MONSTER)
                 .with(new CollidableComponent(true))
-                .with(new monsterComponent("bug",4,15))
+                .with(new monsterComponent("Bug",4,15))
                 .with(new HealthIntComponent(15))
                 .build();
     }
@@ -120,17 +120,17 @@ public class testFactory extends Component implements EntityFactory {
                 .viewWithBBox("chicken.png")
                 .type(testTypes.MONSTER)
                 .with(new CollidableComponent(true))
-                .with(new monsterComponent("chicken",2,10))
+                .with(new monsterComponent("Chicken",2,10))
                 .with(new HealthIntComponent(10))
                 .build();
     }
-    @Spawns("dinosaur")
-    public Entity newDinosaur(SpawnData data){
+    @Spawns("dino")
+    public Entity newDino(SpawnData data){
         return FXGL.entityBuilder(data)
-                .viewWithBBox("dinosaur.png")
+                .viewWithBBox("dino.png")
                 .type(testTypes.MONSTER)
                 .with(new CollidableComponent(true))
-                .with(new monsterComponent("dinosaur",4,19))
+                .with(new monsterComponent("Dino",4,19))
                 .with(new HealthIntComponent(19))
                 .build();
     }
